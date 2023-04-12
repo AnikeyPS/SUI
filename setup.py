c32e58c42b6f6d8be10e49ac6d19f389550b8708
Anikey Semenov <semenov.anikey@gmail.com>
2023-04-12 12:59:19 +0300

    Setup file created

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="SUI",
+    version='1.0',
+    description='A windowed python setup interface',
+    author='AnyKeyPS',
+    author_email='semenov.anikey@gmail.com',
+    packages=['SUI'],
+    package_dir={'SUI': 'SUI'}
+)
